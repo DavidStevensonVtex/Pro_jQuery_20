c7cd465d97fc2a1a65f5c0d12d3e8c0049b6aa34
David Stevenson <drstevenson1958@yahoo.com>
2024-02-04 15:55:27 -0500

    Listing 14-18. The contents of the myscript.js file
    Listing 14-19. Using the getScript method

--- ch14/UsingAjaxPart1/Content/myscript.js
+++ ch14/UsingAjaxPart1/Content/myscript.js
@@ -4,24 +4,25 @@
 	["Carnation", "Lily", "Orchid"]
 ];
 
-$("<div id='row3', class='drow' />").appendTo("div.dtable");
+$("<div id=row3 class=drow />").appendTo("div.dtable");
 
 for (let row = 0; row < flowers.length; row++) {
 
-    let fNames = flowers[row];
-    let fRow = $("< div id='#row" + (row + 1) + "' />");
+	let fNames = flowers[row];
+	let div = "<div id='#row" + (row + 1) + "'></div>";
+	let fRow = $(div);
 
-    for (let i = 0; i < fNames.length; i++) {
-        let img = $("<img />").attr("src", fNames[i] + ".png");
-        let label = $("<label />").attr("for", fNames[i]).text(fNames[i] + ":");
-        let input = $("<input />")
-            .attr("name", fNames[i])
-            .attr("value", 0)
-            .attr("required", "required");
+	for (let i = 0; i < fNames.length; i++) {
+	    let img = $("<img />").attr("src", fNames[i] + ".png");
+	    let label = $("<label />").attr("for", fNames[i]).text(fNames[i] + ":");
+	    let input = $("<input />")
+	        .attr("name", fNames[i])
+	        .attr("value", 0)
+	        .attr("required", "required");
 
-        let cell = $("<div class='dcell'>").append(img).append(label).append(input);
-        fRow.append(cell);
-    }
+	    let cell = $("<div class='dcell'>").append(img).append(label).append(input);
+	    fRow.append(cell);
+	}
 
-    dRow.appendTo("div.dtable");
+	fRow.appendTo("div.dtable");
 }
